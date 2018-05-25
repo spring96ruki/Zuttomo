58215fc3ade36116cea3b9aeee0a72a486551e13
spring96ruki <jiru_spring@yahoo.co.jp>
2018-05-25 13:54:24 +0900

    [Screen] FadeInとFadeOutのメソッド別け、よく分からない

--- friendForever/Assets/Zuttomo/Scripts/kamisato/SelectController.cs
+++ friendForever/Assets/Zuttomo/Scripts/kamisato/SelectController.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SelectController : MonoBehaviour {
+
     //鬼か人間かの選択結果を格納
-    public int[] allplayer_selectstate = new int[4];
+    public int[] allplayerSelectState = new int[4];
     private int player_count = 0;
     private int demon_count = 0;
-    
-
-
 
     // Use this for initialization
     void Start () {
-        allplayer_selectstate = new int[] { 0, 0, 0, 0 };
+        allplayerSelectState = new int[] { 0, 0, 0, 0 };
     }
 
     public void Lottery()
@@ -23,11 +21,11 @@ public class SelectController : MonoBehaviour {
         for (int i = 0; i < 4; i++)
         {
             //プレイヤーの数を数えてカウントする
-            if (allplayer_selectstate[i] != 0)
+            if (allplayerSelectState[i] != 0)
             {
                 player_count++;
                 //鬼の数を数える
-                if (allplayer_selectstate[i] == 2)
+                if (allplayerSelectState[i] == 2)
                 {
                     demon_count++;
                 }
@@ -39,7 +37,7 @@ public class SelectController : MonoBehaviour {
             {
                 case 0:
                     //ランダムで鬼にする
-                    allplayer_selectstate[new System.Random().Next(4)] = 2;
+                    allplayerSelectState[new System.Random().Next(4)] = 2;
                     break;
 
                 case 1:
@@ -49,12 +47,12 @@ public class SelectController : MonoBehaviour {
                     while (demon_count >= 2)
                     {
                         //鬼が二人以上いたらランダムで誰かを人間にする
-                        allplayer_selectstate[new System.Random().Next(4)] = 1;
+                        allplayerSelectState[new System.Random().Next(4)] = 1;
                         demon_count = 0;
                         for (int i = 0; i < 4; i++)
                         {
                             //鬼の数を数える
-                            if (allplayer_selectstate[i] == 2)
+                            if (allplayerSelectState[i] == 2)
                             {
                                 demon_count++;
                             }
@@ -64,12 +62,12 @@ public class SelectController : MonoBehaviour {
             }
             for (int i = 0; i < 4; i++)
             {
-                if (allplayer_selectstate[i] == 2)
+                if (allplayerSelectState[i] == 2)
                 {
                     Debug.Log("鬼" + (i + 1));
                 }
             }
-            FadeManager.Instance.LoadScene(SceneName.GAME_SCENE, 1.0f);
+            SceneController.Instance.LoadScene(SceneName.GAME_SCENE);
         }
     }
 
