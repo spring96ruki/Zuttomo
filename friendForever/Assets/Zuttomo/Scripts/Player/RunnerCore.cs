015ff6a50cd1551e9e8e5e488af8ded50958ae5e
岩崎駿 <25253535.syun718@gmail.com>
2018-06-07 12:35:39 +0900

    プレイヤー生成の部分、不具合一部解決

--- friendForever/Assets/Zuttomo/Scripts/Player/RunnerCore.cs
+++ friendForever/Assets/Zuttomo/Scripts/Player/RunnerCore.cs
@@ -9,20 +9,23 @@ public class RunnerCore : MonoBehaviour {
     public static int m_getdemonNum;
     [SerializeField, Header("プレイヤーの番号")]
     public int runnerNum;
+	public GameObject runner;
+	public GameObject demon;
 
     // Use this for initialization
-    void Start() { 
+    void Start() {
         m_status = GetComponent<RunnerStatus>();
         m_rigidbody = GetComponent<Rigidbody>();
         int m_getdemonNum = SelectController.Getdemonplayer();
+		Debug.Log ("demon:"+this);
         if (m_getdemonNum == runnerNum)
         {
-			GameObject.Find("runner" + runnerNum).SetActive(false);
+			runner.SetActive(true);
 			Debug.Log("プレイヤー" + runnerNum + ":demon");
         }
         else
         {
-            //GameObject.Find("testChaser" + runnerNum).SetActive(false);
+			demon.SetActive(true);
             Debug.Log("プレイヤー" + runnerNum + ":runner");
         }
         
--- friendForever/Assets/Zuttomo/Scripts/Player/RunnerInput.cs
+++ friendForever/Assets/Zuttomo/Scripts/Player/RunnerInput.cs
@@ -47,7 +47,7 @@ public class RunnerInput : MonoBehaviour
                 break;
 
             case 4:
-                
+			EscapePlayerInput();
                 break;
         }
 
