c9bcdcaa9f741efa41a1525268f3f792286c478e
岩崎駿 <25253535.syun718@gmail.com>
2018-07-03 19:52:04 +0900

    リザルト完成度90%

--- friendForever/Assets/Zuttomo/Scripts/Result/ResultController.cs
+++ friendForever/Assets/Zuttomo/Scripts/Result/ResultController.cs
@@ -3,76 +3,92 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ResultController : MonoBehaviour {
-	public float[,] ResultStatus = new float[4, 3];
+    public float[,] ResultStatus = new float[4, 3];
     public int Runner_EscapeCount;
     public int Runner_DeathCount;
     public int Runner_PCount;
     public GameObject TimeController;
+    private int GetChaserNum;
+    public bool RunnerEndFlag;
+    public string[] ResultString = new string[3];
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        int GetChaserNum = SelectController.GetChaserplayer();
         ResultStatus = new float[,]{
             { 0, 0, 0},
             { 0, 0, 0},
             { 0, 0, 0},
             { 0, 0, 0},
         };
-
         //Runnerの場合{捕まるまで、脱出までにかかった時間,何番目に逃げたか,何番目に捕まったか}
-        //Chaserの場合{ゲーム終了までにかかった時間,最終的に何人捕まえたか,0}
+        //Chaserの場合{ゲーム終了までにかかった時間,何人逃げられたか,最終的に何人捕まえたか}
 
         TimeController = GameObject.Find("TimeController");
 
-        Runner_EscapeCount = 1;
-        Runner_DeathCount = 1;
+        Runner_EscapeCount = 0;
+        Runner_DeathCount = 0;
+        Runner_PCount = 1;
 }
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKey("1"))
-        {
-            Runner_PCount = 1;
+        if (Input.GetKeyDown("z")) {
+            RunnerEnd(Runner_PCount, RunnerEndFlag = true);
         }
-        if (Input.GetKey("2"))
+        if (Input.GetKeyDown("x"))
         {
-            Runner_PCount = 2;
+            RunnerEnd(Runner_PCount, RunnerEndFlag = false);
         }
-        if (Input.GetKey("z"))
-        {
-            Debug.Log(GameObject.Find("TimeController").GetComponent<TimeController>().time);
-        }
-        if (Input.GetKey("z"))
-        {
-            Debug.Log(GameObject.Find("TimeController").GetComponent<TimeController>().time);
-        }
-        if (Input.GetKey("x"))
-        {
-            RunnerEscape(Runner_PCount);
-        }
-        if (Input.GetKey("c"))
-        {
-            RunnerDeath(Runner_PCount);
-        }
-        if (Input.GetKey("v"))
-        {
-            Debug.Log(ResultStatus[0, 0]);
-        }
-
-        Runner_PCount = 0;
     }
 
-    public void RunnerEscape(int Count)
+    public void RunnerEnd(int Count, bool flag)
     {
-        Runner_EscapeCount++;
-        Debug.Log(Count);
-    }
-
-    public void RunnerDeath(int Count)
-    {
+        if (flag == true)
+        {
+            Runner_EscapeCount++;
+            ResultStatus[Count - 1, 1] = Runner_EscapeCount;
+        }
+        else
+        {
+            Runner_DeathCount++;
+            ResultStatus[Count - 1, 2] = Runner_DeathCount;
+        }
         ResultStatus[Count - 1, 0] = GameObject.Find("TimeController").GetComponent<TimeController>().time;
-        ResultStatus[Count - 1, 2] = Runner_DeathCount;
-        Debug.Log(Count + ":" + ResultStatus[Count - 1, 0]);
-        Runner_DeathCount++;
-        Debug.Log(Count);
+
+        ChaserResult();
+        ResultOn(Count);
     }
+
+    public void ChaserResult()
+    {
+        if (Runner_DeathCount + Runner_EscapeCount == 3)
+        {
+            ResultStatus[GetChaserNum, 0] = GameObject.Find("TimeController").GetComponent<TimeController>().time;
+            ResultStatus[GetChaserNum, 1] = Runner_EscapeCount;
+            ResultStatus[GetChaserNum, 2] = Runner_DeathCount;
+        }
+    }
+
+    public void ResultOn(int Count)
+    {
+        if (Count - 1 != GetChaserNum)
+        {
+            //Runnerのリザルト表示
+            ResultString[0] = "かかった時間";
+            ResultString[1] = "何番目に逃げたか";
+            ResultString[2] = "何番目に捕まったか";
+        }
+        else
+        {
+            //Chaserのリザルト表示
+            ResultString[0] = "かかった時間";
+            ResultString[1] = "何人に逃げられたか";
+            ResultString[2] = "何人捕まえたか";
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            Debug.Log(ResultString[i] + ResultStatus[Count - 1, i]);
+        }}
 }
